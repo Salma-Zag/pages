137551e49e177d89f42e98c787a4965cb46aeb1f
Salma-Zag <belugafishy1@gmail.com>
2025-10-23 09:40:44 -0700

    image add?

--- assets/js/mansionGame/mansionLevel1.js
+++ assets/js/mansionGame/mansionLevel1.js
@@ -1,2 +1,10 @@
 import GameEnvBackground from './GameEngine/<path>.js'
 import AnimatedPlayer from './GameEngine/<path>.js'
+
+const img = document.createElement("parlor");
+
+// Set its source (make sure the file path is correct)
+img.src = "images/parlor.png";
+
+// Add it to the page
+document.getElementById("parlor").appendChild(img);
