9aa6a812218c9a765abd95e7a6e0770f8fb8ab00
samarthhande <samarthphande@gmail.com>
2025-10-30 11:46:32 -0700

    test using gameEnv.path to fix issue on deployed version

--- assets/js/mansionGame/CustomGameClasses/Projectile.js
+++ assets/js/mansionGame/CustomGameClasses/Projectile.js
@@ -8,6 +8,9 @@ class Projectile extends Character {
         this.target_coords = { x: targetx, y: targety };
         this.type = type;
 
+        // Get the main path
+        const path = gameEnv.path;
+
         // Calculate angle and velocity to move in a straight line
         const dx = targetx - sourcex;
         const dy = targety - sourcey;
@@ -23,14 +26,14 @@ class Projectile extends Character {
         // Load sprite/image based on type
         if (type === "ARROW") {
             this.spriteSheet = new Image();
-            this.spriteSheet.src = "../../images/mansionGame/arrow.png";
+            this.spriteSheet.src = path + "/images/mansionGame/arrow.png";
             this.frameIndex = 0;
             this.frameCount = 1; // single frame
             this.width = 60; // scale down if needed
             this.height = 25;
         } else if (type === "FIREBALL") {
             this.spriteSheet = new Image();
-            this.spriteSheet.src = "../../images/mansionGame/fireball.png";
+            this.spriteSheet.src = path + "/images/mansionGame/fireball.png";
             this.frameIndex = 0;
             this.frameCount = 4; // 2x2 grid
             this.frameCols = 2;
